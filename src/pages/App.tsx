3520c9c986ef0f3219cf48ebbddcf9f4aecb0c6e
jeannguyen <giang.nguyen@arctx.co>
2024-08-15 11:54:34 +0700

    globalCMS: init repo and install required libs

---
+++ src/pages/App.tsx
@@ -0,0 +1,11 @@
+import AppLayout from '@/layout/AppLayout';
+
+function App() {
+  return (
+    <AppLayout>
+      <h1>Vitejs + React + TypeScript + Rudux Thunk + Material UI Boilerplate</h1>
+    </AppLayout>
+  )
+}
+
+export default App
